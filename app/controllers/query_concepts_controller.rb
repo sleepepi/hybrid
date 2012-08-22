4d1d92f19eeceffa9a69148c00417886844ddaa9
Remo Mueller <remosm@gmail.com>
2012-08-22 17:41:42 -0400

    Added Query Concept Controller and Tests

---
+++ app/controllers/query_concepts_controller.rb
@@ -0,0 +1,167 @@
+class QueryConceptsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def select_all
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    if @query
+      @query.query_concepts.update_all(selected: (params[:selected] == 'true'))
+      @query.reload
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def mark_selected
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    @query_concept = @query.query_concepts.find_by_id(params[:query_concept_id]) if @query
+    @query_concept.update_attribute :selected, params[:selected] if @query and @query_concept
+    render nothing: true
+  end
+
+  def copy_selected
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    if @query and @query.query_concepts.where(selected: true).size > 0
+      @query.query_concepts.where(selected: true).each do |query_concept|
+
+        qc_attributes = query_concept.attributes
+        qc_attributes.delete(:id)
+        qc_attributes.delete(:created_at)
+        qc_attributes.delete(:updated_at)
+        qc_attributes[:position] = @query.query_concepts.size
+        qc_attributes[:selected] = false
+        @query.query_concepts.create(qc_attributes)
+      end
+
+      @query.update_brackets!
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def trash_selected
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    if @query and @query.query_concepts.where(selected: true).size > 0
+      @query.query_concepts.where(selected: true).destroy_all
+      @query.update_brackets!
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def indent
+    params[:indent] = 0 unless (-2..2).to_a.include?(params[:indent].to_i)
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    if @query and @query.query_concepts.where(selected: true).size > 0
+      @query.query_concepts.where(selected: true).each do |query_concept|
+        query_concept.update_attribute :level, [query_concept.level + params[:indent].to_i, 0].max
+      end
+
+      @query.update_brackets!
+
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def right_operator
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    @query_concept = @query.query_concepts.find_by_id(params[:query_concept_id]) if @query
+    if @query and @query_concept
+      @query_concept.update_attribute :right_operator, params[:right_operator] if QueryConcept::OPERATOR.include?([params[:right_operator], params[:right_operator]])
+      @query.update_brackets!
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def edit
+    @query_concept = QueryConcept.find(params[:id])
+    @query = @query_concept.query if @query_concept and current_user.all_queries.include?(@query_concept.query)
+    @concept = @query_concept.concept if @query_concept
+
+    if @concept and @concept.mappings.size > 0
+      chart_params = {}
+      width = "100%"
+      if @concept.continuous? or @concept.date?
+        chart_params = {title: @concept.human_name, width: width, height: "300px", units: @concept.human_units, legend: 'none', make_selection: true}
+      elsif @concept.categorical? or @concept.boolean?
+        chart_params = {title: @concept.human_name, width: width, height: "250px", make_selection: true}
+      end
+
+      result_hash = @concept.graph_values(current_user, chart_params)
+      @values = result_hash[:values]
+      @categories = result_hash[:categories]
+      @chart_type = result_hash[:chart_type]
+      @chart_element_id = result_hash[:chart_element_id]
+      @stats = result_hash[:stats]
+      @defaults = result_hash[:defaults]
+
+      @mapping = 1
+    end
+
+    render nothing: true unless @query and @query_concept
+  end
+
+  def create
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+    concept = Concept.current.find_by_id(params[:selected_concept_id])
+
+    unless concept or params[:selected_concept_id] =~ /^[0-9]+$/ or params[:selected_concept_id].blank? or not params[:external_key].blank?
+      params[:source_id] = params[:selected_concept_id].split(',').first
+      params[:external_key] = params[:selected_concept_id].split(',')[1..-1].join(',')
+    end
+
+    # external_concept = Concept.new(name: params[:name], totalnum: params[:totalnum], key: params[:key])
+    if @query and concept
+      @query.query_concepts << @query.query_concepts.create(concept_id: concept.id, position: @query.query_concepts.size)
+      @query.reload
+      render 'query_concepts/query_concepts'
+    elsif @query and not params[:source_id].blank? and not params[:external_key].blank?
+      @query.query_concepts << @query.query_concepts.create(external_key: params[:external_key], source_id: params[:source_id], position: @query.query_concepts.size)
+      @query.reload
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+
+  def update
+    @query_concept = QueryConcept.find_by_id(params[:id])
+    @query = @query_concept.query if @query_concept and current_user.all_queries.include?(@query_concept.query)
+    if @query and @query_concept
+      params[:query_concept] = {} if params[:query_concept].blank?
+      params[:query_concept][:right_operator] = 'and' unless QueryConcept::OPERATOR.include?([params[:query_concept][:right_operator], params[:query_concept][:right_operator]])
+      if @query_concept.concept and @query_concept.concept.date?
+        params[:start_date] = Date.strptime(params[:start_date], "%m/%d/%Y") unless params[:start_date].blank?
+        params[:end_date] = Date.strptime(params[:end_date], "%m/%d/%Y") unless params[:end_date].blank?
+        params[:query_concept][:value] = "#{params[:start_date]}:#{params[:end_date]}"
+      elsif params[:value_ids].blank? and @query_concept.concept and (@query_concept.concept.categorical? or @query_concept.concept.boolean?)
+        params[:query_concept][:value] = ''
+      elsif not params[:value_ids].blank?
+        params[:query_concept][:value] = params[:value_ids].keys.join(',')
+      end
+      @query_concept.update_attributes(params[:query_concept])
+      render 'query_concepts/query_concepts'
+      # render "show"
+    else
+      render nothing: true
+    end
+  end
+
+  def destroy
+    @query_concept = QueryConcept.find_by_id(params[:id])
+    @query = @query_concept.query if @query_concept and current_user.all_queries.include?(@query_concept.query)
+    if @query and @query_concept
+      @query_concept.destroy
+      @query.update_brackets!
+      render 'query_concepts/query_concepts'
+    else
+      render nothing: true
+    end
+  end
+end
---
+++ test/functional/query_concepts_controller_test.rb
@@ -0,0 +1,233 @@
+require 'test_helper'
+
+class QueryConceptsControllerTest < ActionController::TestCase
+  setup do
+    login(users(:admin))
+    @query_concept = query_concepts(:one)
+  end
+
+  test "should mark all query concepts as selected" do
+    post :select_all, query_id: queries(:one).to_param, selected: 'true', format: 'js'
+
+    assert_equal assigns(:query).query_concepts.size, assigns(:query).query_concepts.where(selected: true).size
+    assert_template 'query_concepts'
+  end
+
+  test "should not mark query concepts as selected for invalid query" do
+    post :select_all, query_id: -1, selected: 'true', format: 'js'
+    assert_nil assigns(:query)
+    assert_response :success
+  end
+
+  test "should clear selection of all query concepts" do
+    post :select_all, query_id: queries(:one).to_param, selected: 'false', format: 'js'
+
+    assert_equal assigns(:query).query_concepts.size, assigns(:query).query_concepts.where(selected: false).size
+    assert_template 'query_concepts'
+  end
+
+  test "should mark individual query concept as selected" do
+    post :mark_selected, query_id: queries(:one).to_param, query_concept_id: query_concepts(:one).to_param, selected: 'true', format: 'js'
+    assert assigns(:query)
+    assert assigns(:query_concept)
+    assert_equal true, assigns(:query_concept).selected
+    assert_response :success
+  end
+
+  test "should clear selection for individual query concept" do
+    post :mark_selected, query_id: queries(:one).to_param, query_concept_id: query_concepts(:categorical), selected: 'false', format: 'js'
+    assert assigns(:query)
+    assert assigns(:query_concept)
+    assert_equal false, assigns(:query_concept).selected
+    assert_response :success
+  end
+
+  test "should copy selected query concepts and append to end of query" do
+    assert_difference('queries(:one).query_concepts.size', queries(:one).query_concepts.where(selected: true).size) do
+      post :copy_selected, query_id: queries(:one).to_param, format: 'js'
+    end
+
+    assert_template 'query_concepts'
+  end
+
+  test "should copy no query concepts if none are selected" do
+    post :select_all, query_id: queries(:one).to_param, selected: 'false', format: 'js'
+    assert_response :success
+
+    assert_difference('queries(:one).query_concepts.size', 0) do
+      post :copy_selected, query_id: queries(:one).to_param, format: 'js'
+    end
+
+    assert_response :success
+  end
+
+  test "should trash selected query concepts" do
+    assert_difference('queries(:one).query_concepts.size', -1*queries(:one).query_concepts.where(selected: true).size) do
+      post :trash_selected, query_id: queries(:one).to_param, format: 'js'
+    end
+
+    assert_equal 0, assigns(:query).query_concepts.where(selected: true).size
+    assert_template 'query_concepts'
+  end
+
+  test "should leave query untouched if no query concepts are selected" do
+    post :select_all, query_id: queries(:one).to_param, selected: 'false', format: 'js'
+    assert_response :success
+
+    assert_difference('queries(:one).query_concepts.count', 0) do
+      post :trash_selected, query_id: queries(:one).to_param, format: 'js'
+    end
+
+    assert_response :success
+  end
+
+  test "should indent selected query concepts" do
+    indent = 1
+    assert_difference('queries(:one).query_concepts.where(selected: true).collect(&:level).sum', indent * queries(:one).query_concepts.where(selected: true).size ) do
+      post :indent, query_id: queries(:one).to_param, indent: indent, format: 'js'
+    end
+
+    assert assigns(:query)
+    assert_template 'query_concepts'
+  end
+
+  test "should not indent selected query concepts with invalid query" do
+    post :indent, query_id: -1, indent: 1, format: 'js'
+
+    assert_nil assigns(:query)
+    assert_response :success
+  end
+
+  test "should update the right operator for a query concept" do
+    right_operator = 'or'
+    post :right_operator, query_id: queries(:one).to_param, query_concept_id: query_concepts(:one), right_operator: right_operator, format: 'js'
+    assert assigns(:query)
+    assert assigns(:query_concept)
+    assert right_operator, assigns(:query_concept).right_operator
+    assert_template 'query_concepts'
+  end
+
+  test "should not update the right operator if an invalid right operator is given for a query concept" do
+    right_operator = 'nand'
+    post :right_operator, query_id: queries(:one).to_param, query_concept_id: query_concepts(:one), right_operator: right_operator, format: 'js'
+    assert assigns(:query)
+    assert assigns(:query_concept)
+    assert_not_equal right_operator, assigns(:query_concept).right_operator
+    assert_template 'query_concepts'
+  end
+
+  test "should not update the right operator for a query concept with invalid query" do
+    right_operator = 'or'
+    post :right_operator, query_id: -1, query_concept_id: query_concepts(:one), right_operator: right_operator, format: 'js'
+    assert_nil assigns(:query)
+    assert_response :success
+  end
+
+  test "should create query concept" do
+    assert_difference('QueryConcept.count') do
+      post :create, query_concept: @query_concept.attributes, query_id: queries(:one).to_param, selected_concept_id: concepts(:continuous_with_label).to_param, format: 'js'
+    end
+
+    assert_not_nil assigns(:query)
+    assert_template 'query_concepts'
+  end
+
+  test "should create query concept for external concept" do
+    assert_difference('QueryConcept.count') do
+      post :create, query_concept: @query_concept.attributes, external_key: 'external_key', source_id: sources(:three).to_param, query_id: queries(:one).to_param, format: 'js'
+    end
+
+    assert_not_nil assigns(:query)
+    assert_template 'query_concepts'
+  end
+
+  test "should not create query concept without valid concept" do
+    assert_difference('QueryConcept.count', 0) do
+      post :create, query_concept: @query_concept.attributes, query_id: queries(:one).to_param, selected_concept_id: -1, format: 'js'
+    end
+
+    assert_not_nil assigns(:query)
+    assert_response :success
+  end
+
+  test "should get edit for a continuous query concept" do
+    get :edit, id: @query_concept.to_param, format: 'js'
+    assert_not_nil assigns(:query_concept)
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:values)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:chart_type)
+    assert_not_nil assigns(:chart_element_id)
+    assert_not_nil assigns(:stats)
+    assert_not_nil assigns(:defaults)
+    assert_template 'edit'
+  end
+
+  test "should get edit for a categorical query concept" do
+    get :edit, id: query_concepts(:categorical).to_param, format: 'js'
+    assert_not_nil assigns(:query_concept)
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:values)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:chart_type)
+    assert_not_nil assigns(:chart_element_id)
+    assert_not_nil assigns(:stats)
+    assert_not_nil assigns(:defaults)
+    assert_template 'edit'
+  end
+
+  test "should update query concept" do
+    put :update, id: @query_concept.to_param, query_concept: @query_concept.attributes, format: 'js'
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:query_concept)
+    assert_template 'query_concepts'
+  end
+
+  test "should update query concept for categorical concept" do
+    put :update, id: query_concepts(:categorical).to_param, query_concept: query_concepts(:categorical).attributes, format: 'js'
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:query_concept)
+    assert_template 'query_concepts'
+  end
+
+  test "should update query concept for categorical concept with value ids" do
+    put :update, id: query_concepts(:categorical).to_param, query_concept: query_concepts(:categorical).attributes, value_ids: { '11' => '1', '14' => '1' }, format: 'js' # TODO: Replace hardcoded values
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:query_concept)
+    assert_template 'query_concepts'
+  end
+
+  test "should update query concept for date concept" do
+    put :update, id: query_concepts(:date_concept).to_param, query_concept: query_concepts(:date_concept).attributes, start_date: '01/01/2011', format: 'js'
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:query_concept)
+    assert_template 'query_concepts'
+  end
+
+  test "should not update query concept with invalid id" do
+    put :update, id: -1, query_concept: @query_concept.attributes, format: 'js'
+    assert_nil assigns(:query)
+    assert_nil assigns(:query_concept)
+    assert_response :success
+  end
+
+  test "should destroy query concept" do
+    assert_difference('QueryConcept.current.count', -1) do
+      delete :destroy, id: @query_concept.to_param, format: 'js'
+    end
+
+    assert_not_nil assigns(:query)
+    assert_template 'query_concepts'
+  end
+
+  test "should not destroy query concept with invalid id" do
+    assert_difference('QueryConcept.count', 0) do
+      delete :destroy, id: -1, format: 'js'
+    end
+    assert_nil assigns(:query_concept)
+    assert_nil assigns(:query)
+    assert_response :success
+  end
+end
