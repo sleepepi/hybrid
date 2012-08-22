4f90702bae8970e6a0e0a2250b1af69f5de8fa25
Remo Mueller <remosm@gmail.com>
2012-08-22 17:36:00 -0400

    Added Concept Controller and tests

---
+++ app/controllers/concepts_controller.rb
@@ -0,0 +1,169 @@
+class ConceptsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def info
+    @concept = Concept.find_by_id(params[:id])
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+
+    if @concept and @concept.mappings.size > 0
+      chart_params = {}
+      width = "100%"
+      if @concept.continuous? or @concept.date?
+        chart_params = { title: @concept.human_name, width: width, height: "300px", units: @concept.human_units, legend: 'none' }
+      elsif @concept.categorical? or @concept.boolean?
+        chart_params = { title: @concept.human_name, width: width, height: "250px" }
+      end
+
+      result_hash = @concept.graph_values(current_user, chart_params)
+      @values = result_hash[:values]
+      @categories = result_hash[:categories]
+      @chart_type = result_hash[:chart_type]
+      @chart_element_id = result_hash[:chart_element_id]
+      @stats = result_hash[:stats]
+      @defaults = result_hash[:defaults]
+
+      @mapping = 1
+    end
+
+    render nothing: true unless @concept
+  end
+
+  def index
+    # current_user.update_attribute :users_per_page, params[:users_per_page].to_i if params[:users_per_page].to_i >= 10 and params[:users_per_page].to_i <= 200
+    @order = params[:order].blank? ? 'concepts.search_name' : params[:order]
+    concept_scope = Concept.current
+    @query = current_user.queries.find_by_id(params[:query_id])
+
+    # @first_terms = params[:term].to_s.split(',')[0..-2]
+    # @search_terms = params[:term].to_s.split(',').last.strip.gsub(/[^0-9a-zA-Z]/, ' ').split(' ')
+
+    concept_scope = concept_scope.with_concept_type(params[:concept_type] || 'all')
+    search_string = (params[:search] || params[:term] || params[:concept_search_term]).to_s.strip
+    @search_terms = search_string.gsub(/[^0-9a-zA-Z]/, ' ').split(' ')
+    @search_terms.each{|search_term| concept_scope = concept_scope.search(search_term) }
+    if params[:autocomplete] == 'true'
+      concept_scope = concept_scope.with_source(@query.sources.collect{|s| s.all_linked_sources_and_self}.flatten.uniq.collect{|s| s.id}) if @query
+      concept_scope = concept_scope.order("(concepts.folder IS NULL or concepts.folder = '') ASC, concepts.folder ASC, " + @order)
+      @concepts = concept_scope.page(params[:page]).per(10)
+
+      @external_concepts = []
+      if @query and not search_string.blank?
+        @query.sources.each do |source|
+          external_concepts_hash = source.external_concepts(current_user, '', search_string)
+          if external_concepts_hash[:error].blank?
+            @external_concepts = @external_concepts | external_concepts_hash[:result]
+          end
+        end
+      end
+
+      render 'autocomplete'
+    else
+      concept_scope = concept_scope.with_dictionary(params[:dictionary_id].blank? ? 'all' : params[:dictionary_id]).order(@order)
+      @concepts = concept_scope.page(params[:page]).per(20) #(current_user.concepts_per_page)
+      @dictionary = Dictionary.available.find_by_id(params[:dictionary_id])
+    end
+  end
+
+  def search_folder
+    params['concept_search'] = params['search'] if params['concept_search'].blank?
+    @query = current_user.queries.find_by_id(params[:query_id])
+    @query = current_user.queries.new if @query.blank?
+    render 'popup' if params[:popup] == 'true'
+  end
+
+  def open_folder
+    @query = current_user.queries.find_by_id(params[:query_id])
+    @folder = params[:folder_name]
+    @r = Regexp.new("^#{params[:prefix].to_s.gsub('\\','\\\\\\\\')}:")
+    @query = current_user.queries.new if @query.blank?
+    render nothing: true unless @query
+  end
+
+  def info_external
+    @concept = Concept.new(name: params[:name], totalnum: params[:totalnum], key: params[:key], source_id: params[:source_id])
+    @query_concept = QueryConcept.new(external_key: params[:key], source_id: params[:source_id])
+    @query = current_user.all_queries.find_by_id(params[:query_id])
+
+    @mapping = nil
+
+    render 'info'
+  end
+
+  # # GET /concepts
+  # # GET /concepts.xml
+  # def index
+  #   @concepts = Concept.all
+  #
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.xml  { render xml: @concepts }
+  #   end
+  # end
+
+  def show
+    @concept = Concept.find_by_id(params[:id])
+    unless @concept
+      redirect_to concepts_path
+    end
+  end
+
+  # # GET /concepts/new
+  # # GET /concepts/new.xml
+  # def new
+  #   @concept = Concept.new
+  #
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.xml  { render xml: @concept }
+  #   end
+  # end
+  #
+  # # GET /concepts/1/edit
+  # def edit
+  #   @concept = Concept.find(params[:id])
+  # end
+  #
+  # # POST /concepts
+  # # POST /concepts.xml
+  # def create
+  #   @concept = Concept.new(params[:concept])
+  #
+  #   respond_to do |format|
+  #     if @concept.save
+  #       format.html { redirect_to(@concept, notice: 'Concept was successfully created.') }
+  #       format.xml  { render xml: @concept, status: :created, location: @concept }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.xml  { render xml: @concept.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # PUT /concepts/1
+  # # PUT /concepts/1.xml
+  # def update
+  #   @concept = Concept.find(params[:id])
+  #
+  #   respond_to do |format|
+  #     if @concept.update_attributes(params[:concept])
+  #       format.html { redirect_to(@concept, notice: 'Concept was successfully updated.') }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.xml  { render xml: @concept.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /concepts/1
+  # # DELETE /concepts/1.xml
+  # def destroy
+  #   @concept = Concept.find(params[:id])
+  #   @concept.destroy
+  #
+  #   respond_to do |format|
+  #     format.html { redirect_to(concepts_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end
+end
---
+++ test/functional/concepts_controller_test.rb
@@ -0,0 +1,148 @@
+require 'test_helper'
+
+class ConceptsControllerTest < ActionController::TestCase
+  setup do
+    login(users(:valid))
+    @concept = concepts(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:concepts)
+  end
+
+  test "should get autocomplete" do
+    get :index, autocomplete: 'true', format: 'js'
+    assert_not_nil assigns(:concepts)
+    assert_template 'autocomplete'
+  end
+
+  test "should get autocomplete with external concepts" do
+    get :index, autocomplete: 'true', concept_search_term: 'search term', query_id: queries(:query_with_sources).to_param, format: 'js'
+    assert_not_nil assigns(:concepts)
+    assert_template 'autocomplete'
+  end
+
+  test "should get info for categorical concept" do
+    post :info, id: concepts(:categorical).to_param, query_id: queries(:three).to_param, format: 'js'
+
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:values)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:chart_type)
+    assert_not_nil assigns(:chart_element_id)
+    assert_not_nil assigns(:stats)
+    assert_not_nil assigns(:defaults)
+    assert_not_nil assigns(:mapping)
+
+    assert_template 'info'
+  end
+
+  test "should get info for continuous concept" do
+    post :info, id: concepts(:continuous).to_param, query_id: queries(:one).to_param, format: 'js'
+
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:values)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:chart_type)
+    assert_not_nil assigns(:chart_element_id)
+    assert_not_nil assigns(:stats)
+    assert_not_nil assigns(:defaults)
+    assert_not_nil assigns(:mapping)
+
+    assert_template 'info'
+  end
+
+  test "should get info for continuous concept with formula" do
+    post :info, id: concepts(:formula).to_param, query_id: queries(:one).to_param, format: 'js'
+
+    assert_not_nil assigns(:query)
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:values)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:chart_type)
+    assert_not_nil assigns(:chart_element_id)
+    assert_not_nil assigns(:stats)
+    assert_not_nil assigns(:defaults)
+    assert_not_nil assigns(:mapping)
+
+    assert_template 'info'
+  end
+
+  test "should get info and render blank without concept" do
+    post :info, id: -1, query_id: queries(:three).to_param, format: 'js'
+    assert_nil assigns(:concept)
+    assert_response :success
+  end
+
+  test "search folder" do
+    post :search_folder, id: concepts(:categorical).to_param, query_id: queries(:three).to_param, format: 'js'
+    assert_not_nil assigns(:query)
+    assert_template 'search_folder'
+  end
+
+  test "open folder" do
+    post :open_folder, id: concepts(:categorical).to_param, query_id: queries(:three).to_param, format: 'js'
+    assert_not_nil assigns(:query)
+    assert_template 'open_folder'
+  end
+
+  test "search folder opens popup" do
+    post :search_folder, id: concepts(:categorical).to_param, query_id: queries(:three).to_param, popup: 'true', format: 'js'
+    assert_not_nil assigns(:query)
+    assert_template 'popup'
+  end
+
+  test "should get external concept info" do
+    post :info_external, query_id: queries(:three).to_param, name: 'Concept Name', totalnum: 5, key: '//concept/key', source_id: sources(:one).to_param, format: 'js'
+    assert_not_nil assigns(:concept)
+    assert_not_nil assigns(:query)
+    assert_nil assigns(:mapping)
+    assert_template 'info'
+  end
+
+  # TODO: Test Concept Creation, Deletion, etc
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+  #
+  # test "should create concept" do
+  #   assert_difference('Concept.count') do
+  #     post :create, concept: @concept.attributes
+  #   end
+  #
+  #   assert_redirected_to concept_path(assigns(:concept))
+  # end
+
+  test "should show concept" do
+    get :show, id: concepts(:boolean).to_param
+    assert_response :success
+  end
+
+  test "should redirect if concept is not found" do
+    get :show, id: -1
+    assert_redirected_to concepts_path
+  end
+
+  # test "should get edit" do
+  #   get :edit, id: @concept.to_param
+  #   assert_response :success
+  # end
+  #
+  # test "should update concept" do
+  #   put :update, id: @concept.to_param, concept: @concept.attributes
+  #   assert_redirected_to concept_path(assigns(:concept))
+  # end
+  #
+  # test "should destroy concept" do
+  #   assert_difference('Concept.count', -1) do
+  #     delete :destroy, id: @concept.to_param
+  #   end
+  #
+  #   assert_redirected_to concepts_path
+  # end
+end
