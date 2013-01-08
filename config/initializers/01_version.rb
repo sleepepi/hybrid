a190ba63487b431be7676f938e7c017b43b422cf
Remo Mueller <remosm@gmail.com>
2013-01-08 16:10:08 -0500

    Version bump to 0.12.4.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -2,8 +2,8 @@ module Hybrid
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 12
-    TINY = 3
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    TINY = 4
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
