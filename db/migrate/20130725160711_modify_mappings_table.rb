cc27f6ac655091ad3860a5f615d7e784569a5678
Remo Mueller <remosm@gmail.com>
2013-07-25 15:05:13 -0400

    Add mappings variable_id index earlier

--- db/migrate/20130725160711_modify_mappings_table.rb
+++ db/migrate/20130725160711_modify_mappings_table.rb
@@ -2,6 +2,5 @@ class ModifyMappingsTable < ActiveRecord::Migration
   def change
     remove_column :mappings, :value, :string
     remove_column :mappings, :column_value, :string
-    add_index :mappings, :variable_id
   end
 end
