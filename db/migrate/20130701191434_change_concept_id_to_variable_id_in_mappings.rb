a59a3cd56771ed10d02e9450858a731bd1d011b0
Remo Mueller <remosm@gmail.com>
2013-07-25 15:02:51 -0400

    Modified migration to include index

--- db/migrate/20130701191434_change_concept_id_to_variable_id_in_mappings.rb
+++ db/migrate/20130701191434_change_concept_id_to_variable_id_in_mappings.rb
@@ -1,11 +1,15 @@
 class ChangeConceptIdToVariableIdInMappings < ActiveRecord::Migration
   def up
+    remove_index :mappings, :concept_id
     remove_column :mappings, :concept_id
     add_column :mappings, :variable_id, :integer
+    add_index :mappings, :variable_id
   end
 
   def down
+    remove_index :mappings, :variable_id
     remove_column :mappings, :variable_id
     add_column :mappings, :concept_id, :integer
+    add_index :mappings, :concept_id
   end
 end
