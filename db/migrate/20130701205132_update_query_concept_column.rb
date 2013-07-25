27353b40af9efa42c3b7a404e9a93d8ff0c57601
Remo Mueller <remosm@gmail.com>
2013-07-25 15:09:22 -0400

    Make sure indices are added removed properly for migrations

--- db/migrate/20130701205132_update_query_concept_column.rb
+++ db/migrate/20130701205132_update_query_concept_column.rb
@@ -1,11 +1,15 @@
 class UpdateQueryConceptColumn < ActiveRecord::Migration
   def up
+    remove_index :query_concepts, :concept_id
     remove_column :query_concepts, :concept_id
     add_column :query_concepts, :variable_id, :integer
+    add_index :query_concepts, :variable_id
   end
 
   def down
+    remove_index :query_concepts, :variable_id
     remove_column :query_concepts, :variable_id
     add_column :query_concepts, :concept_id, :integer
+    add_index :query_concepts, :concept_id
   end
 end
--- db/migrate/20130725154545_modify_criteria_table.rb
+++ db/migrate/20130725154545_modify_criteria_table.rb
@@ -4,7 +4,6 @@ class ModifyCriteriaTable < ActiveRecord::Migration
     remove_column :criteria, :group_negated, :boolean, null: false, default: false
     remove_column :criteria, :external_key, :string
 
-    add_index :criteria, :variable_id
     add_index :criteria, :mapping_id
   end
 end
